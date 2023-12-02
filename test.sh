51a685cdef132b2f340ab16569af80554504d74b
ijiefeng <ijiefeng@tencent.com>
2023-12-02 22:51:58 +0800

    Add auto install the node modules

--- test.sh
+++ test.sh
@@ -1,7 +1,13 @@
 echo TypeScript 4.9
 cd ts49
+if [ ! -d "node_modules" ]; then
+  npm install
+fi
 npx tsc --noEmit
 
 echo TypeScript 5.x
 cd ../ts5x
+if [ ! -d "node_modules" ]; then
+  npm install
+fi
 npx tsc --noEmit
